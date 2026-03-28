54638e534c115d545b77ce249410e558624cb10c
Elvis Amaya <esamaya@stanford.edu>
2026-03-27 19:02:24 -0700

    Implemented StatusLedTask() and updated AppTasks_Init() to create the first FreeRTOS thread with a low priority and dedicated stack.

--- Core/Src/app_tasks.c
+++ Core/Src/app_tasks.c
@@ -1,6 +1,26 @@
 #include "app_tasks.h"
+#include "main.h"
+
+osThreadId_t statusLedTaskHandle;
+
+void StatusLedTask(void *argument)
+{
+    (void)argument;
+
+    for (;;)
+    {
+        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+        osDelay(500);
+    }
+}
 
 void AppTasks_Init(void)
 {
-    /* */
+    const osThreadAttr_t statusLedTaskAttr = {
+        .name = "StatusLedTask",
+        .priority = osPriorityLow,
+        .stack_size = 256
+    };
+
+    statusLedTaskHandle = osThreadNew(StatusLedTask, NULL, &statusLedTaskAttr);
 }
