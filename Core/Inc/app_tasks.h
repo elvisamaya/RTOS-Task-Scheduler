7a59de29194fca23b6da7e2f96c0a6cb232533fb
Elvis Amaya <esamaya@stanford.edu>
2026-03-31 00:47:31 -0700

    Added motor task support, included a PWM timer handle in task initialization, and declared Servo_SetAngle() for actuator control.

--- Core/Inc/app_tasks.h
+++ Core/Inc/app_tasks.h
@@ -3,14 +3,18 @@
 
 #include "cmsis_os2.h"
 #include "stm32f4xx_hal.h"
+#include <stdint.h>
 
 extern osThreadId_t statusLedTaskHandle;
 extern osThreadId_t uartTaskHandle;
 extern osThreadId_t sensorTaskHandle;
+extern osThreadId_t motorTaskHandle;
 
-void AppTasks_Init(UART_HandleTypeDef *huart);
+void AppTasks_Init(UART_HandleTypeDef *huart, TIM_HandleTypeDef *htim_pwm);
 void StatusLedTask(void *argument);
 void UartTask(void *argument);
 void SensorTask(void *argument);
+void MotorTask(void *argument);
+void Servo_SetAngle(uint8_t angle);
 
 #endif /* APP_TASKS_H */
