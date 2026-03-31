b133f0ae0cd8fba021227c1065dc42dd7a5f3041
Elvis Amaya <esamaya@stanford.edu>
2026-03-30 21:26:14 -0700

    Extended the task module to include a UART task handle, UART task prototype, and a UART handle passed into task initialization.

--- Core/Inc/app_tasks.h
+++ Core/Inc/app_tasks.h
@@ -2,10 +2,13 @@
 #define APP_TASKS_H
 
 #include "cmsis_os2.h"
+#include "stm32f4xx_hal.h"
 
 extern osThreadId_t statusLedTaskHandle;
+extern osThreadId_t uartTaskHandle;
 
-void AppTasks_Init(void);
+void AppTasks_Init(UART_HandleTypeDef *huart);
 void StatusLedTask(void *argument);
+void UartTask(void *argument);
 
 #endif /* APP_TASKS_H */
