a1dfeb99b620fcee70bf72ff3ac09f570d7a69f9
Elvis Amaya <esamaya@stanford.edu>
2026-04-02 19:11:59 -0700

    Added SensorEvent_t and exposed the sensor queue handle so tasks can communicate through a defined event structure

--- Core/Inc/app_tasks.h
+++ Core/Inc/app_tasks.h
@@ -5,10 +5,17 @@
 #include "stm32f4xx_hal.h"
 #include <stdint.h>
 
+typedef struct
+{
+    uint8_t triggered;
+} SensorEvent_t;
+
 extern osThreadId_t statusLedTaskHandle;
 extern osThreadId_t uartTaskHandle;
 extern osThreadId_t sensorTaskHandle;
 extern osThreadId_t motorTaskHandle;
+
+extern osMessageQueueId_t sensorQueueHandle;
 
 void AppTasks_Init(UART_HandleTypeDef *huart, TIM_HandleTypeDef *htim_pwm);
 void StatusLedTask(void *argument);
